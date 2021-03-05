59e318cf80a3b2be655cec3764a90db84c96aebe
Dylan J Edwards <dylanjedwards@iMac.local>
2021-03-05 10:16:42 -0800

    add action interface to reducer

--- client/src/reducers/counterReducer.ts
+++ client/src/reducers/counterReducer.ts
@@ -1,6 +1,6 @@
-import { createReducer } from "@reduxjs/toolkit";
+import { createReducer, PayloadAction } from "@reduxjs/toolkit";
 import { increment } from "../actions/counter/counterActions";
-
+import { IIncrementAction } from "../actions/counter/counterTypes"
 
 interface CounterState {
   currentValue: number;
@@ -15,8 +15,8 @@ const initialState: CounterState = {
  * @case increment - adds the incremented payload to the store.currentvalue
  */
 const counterReducer = createReducer(initialState, (builder) => {
-  builder.addCase(increment, (state: CounterState, action) => {
-    state.currentValue += action.payload.amount;
+  builder.addCase(increment, (state: CounterState, {payload}: PayloadAction<IIncrementAction>) => {
+    state.currentValue += payload.amount;
   });
 });
 
