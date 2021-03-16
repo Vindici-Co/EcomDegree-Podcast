4398a0ebb08703e654b0c94663aa9293512a4a45
Dylan J Edwards <dylanjedwards@iMac.local>
2021-03-15 17:48:11 -0700

    complete learning card

--- client/src/views/components/LearningCard.tsx
+++ client/src/views/components/LearningCard.tsx
@@ -1,96 +1,116 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {StyledProps} from "styled-components";
+import AmazonIcon from "../Icons/AmazonIcon";
+import ShopifyIcon from "../Icons/ShopifyIcon";
+
 
 /**
  * @Interface ILearningCardProps
- * @cardImg { any } image on left side of card
- * @upperTitle { string } greyed out title at top of card
- * @description { string } middle desc of card
- * @lowerTitle { string }  boldedTitle below description
- * @btnColor { string } color of btn
+ * @field { "amazon" | "shopify" } platformType - the type of card shopify or amazon
+ * @field { string } cardThumbnail - the thumbnail for the given card video
  */
 
 export interface ILearningCardProps {
-	cardImg: any;
-	upperTitle: string;
-	description: string;
-	lowerTitle: string;
-	btnColor: string;
+	platformType: "amazon" | "shopify";
+  cardThumbnail: string;
 }
 
 const Container = styled.div`
-	margin: 50px;
-	display: flex;
-	width: 500px;
-	height: 230px;
-	border-radius: 15px;
-	background: #7482b4;
+  margin: 50px;
+  display: flex;
+  flex-direction: row;
+  width: 45vw;
+  min-width: 650px;
+  height: 400px;
+  border-radius: 15px;
+  background: #192431;
+  justify-content: flex-start;
+  align-self: center;
 `;
 
 const Image = styled.img`
-	height: 230px;
-	width: 250px;
-	border-radius: 15px;
+  height: 100%;
+  width: 50%;
+  border-radius: 15px;
 `;
 
 const TextContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	width: 150px;
-	margin: 0 0 0 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 150px;
+  margin: 0 0 0 50px;
 `;
 
 const UpperTitle = styled.p`
-	font-family: ${props => props.theme.fonts.secondary};
-	font-size: 10px;
-	color: ${props => props.theme.grays[100]};
-	font-weight: lighter;
-	text-align: left;
-	margin: 0 48px 0 0;
+  font-family: ${(props) => props.theme.fonts.secondary};
+  font-size: 10px;
+  color: ${(props) => props.theme.grays[100]};
+  font-weight: lighter;
+  margin: 0 48px 0 0;
+  width: 100%;
 `;
 const Description = styled.p`
-	font-family: ${props => props.theme.fonts.main};
-	font-size: 16px;
-	color: ${props => props.theme.grays[100]};
-	margin: 13px 0 0 6px;
-	font-weight: 500;
-	text-align: left;
-`;
-const LowerTitle = styled.p`
-	font-family: ${props => props.theme.fonts.main};
-	font-size: 17px;
-	color: ${props => props.theme.grays[100]};
-	font-weight: 600;
-	margin: 5px 0 0 6px;
-	text-align: left;
+  font-family: ${(props) => props.theme.fonts.main};
+  font-size: 25px;
+  color: ${(props) => props.theme.grays[100]};
+  font-weight: 500;
+  width: 188px;
+  text-align: left;
 `;
 
-const LearningCard: React.FC<ILearningCardProps> = (props: ILearningCardProps) => {
-	const Button = styled.button`
-		background: ${(p: ILearningCardProps) => p.btnColor};
-		font-family: ${props => props.theme.fonts.main};
-		color: white;
-		border: none;
-		border-radius: 14px;
-		padding: 7px 33px;
-		cursor: pointer;
-		margin: 24px 0 0 0;
-		box-shadow: ${(p: ILearningCardProps) => `-15px 10px 30px ${p.btnColor}`};
-	`;
+const HeadlineContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding-bottom: 46px;
+`;
 
-	return (
-		<Container>
-			<Image src={props.cardImg} alt="image" />
-			<TextContainer>
-				<UpperTitle>{props.upperTitle}</UpperTitle>
-				<Description>{props.description} </Description>
-				<LowerTitle>{props.lowerTitle}</LowerTitle>
-				<Button {...props}>Wach Now</Button>
-			</TextContainer>
-		</Container>
-	);
+const getButtonColor = (props: StyledProps<any>) => {
+	if(props.platformType === "shopify"){
+		return "#5E8E3E"
+	}
+	return "#F49A12"
+};
+
+const Button = styled.button`
+  background: ${(props) => getButtonColor(props)};
+  font-family: ${(props) => props.theme.fonts.main};
+  color: white;
+  width: 180px;
+  height: 50px;
+  border: none;
+  outline: none;
+  border-radius: 14px;
+  cursor: pointer;
+  box-shadow: ${(props) => `-15px 10px 30px ${getButtonColor(props)}`};
+`;
+const LearningCard: React.FC<ILearningCardProps> = (
+  props: ILearningCardProps
+) => {
+  return (
+    <Container>
+      <Image src={props.cardThumbnail} alt="image" />
+      <TextContainer>
+        {props.platformType === "shopify" ? (
+          <UpperTitle>Shopify Droppshipping </UpperTitle>
+        ) : (
+          <UpperTitle>Amazon FBA & FBM</UpperTitle>
+        )}
+        <HeadlineContainer>
+          {props.platformType === "shopify" ? (
+            <Description>Build Your Own Shopify Brand </Description>
+          ) : (
+            <Description>Learn More About Amazon</Description>
+          )}
+          {props.platformType === "shopify" ? <ShopifyIcon /> : <AmazonIcon />}
+        </HeadlineContainer>
+        <Button {...props}>Watch Now</Button>
+      </TextContainer>
+    </Container>
+  );
 };
 
 export default LearningCard;
